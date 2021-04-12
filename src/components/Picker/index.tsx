7451fa21500f9f8ea4056fcabcb6fcaa9d31a854
Alex Hauka <alexhauka@gmail.com>
2021-04-12 14:56:34 -0700

    add options icons

--- src/components/Picker/index.tsx
+++ src/components/Picker/index.tsx
@@ -1,15 +1,37 @@
 import React from 'react';
-import { Container, Preview, Options } from './styles';
+import { Container, Preview, Description, Menu, MenuItem } from './styles';
 import PlaceholderImage from '../../assets/pizza.jpg'
+import {IoIosLeaf, IoMdPizza, IoIosStar } from 'react-icons/io'
+import {IconContext} from "react-icons"
+
+
+
+
+const PlaceholderDescription: string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultricies mi quis hendrerit dolor magna. Cursus mattis molestie a iaculis.'
 
 const Picker: React.FC = () => {
   return (
     <>
       <Container>
         <Preview src={PlaceholderImage}/>
-        <Options>
-          options buttons goes here
-        </Options>
+        <Description>
+        {PlaceholderDescription}
+        </Description>
+        <Menu>
+          <IconContext.Provider value={{
+            style: {fontSize: '30px', color: "white"}
+          }}>
+          <MenuItem>
+            <IoIosLeaf/>
+          </MenuItem>
+          <MenuItem>
+            <IoMdPizza />
+          </MenuItem>
+          <MenuItem>
+            <IoIosStar />
+          </MenuItem>
+          </IconContext.Provider>
+        </Menu>
       </Container>
     </>
   );
--- src/components/Picker/styles.tsx
+++ src/components/Picker/styles.tsx
@@ -10,22 +10,35 @@ export const Container = styled.div`
   min-height: 80%;
   margin: 10%;
   margin-top: 10%;
-  border: 2px solid white;
+  border: 1px solid grey;
   border-radius: 20px;
   background-color: #f1f1f12b;
 `;
 
 export const Preview = styled.img`
-  opacity: 1;
-  width: 80%;
+  width: 90%;
   height: 50%;
-  margin: 10%;
-  border: 2px solid white;
-  border-radius: 15px;
+  margin: 5%;
+  border: 0px solid white;
+  border-radius: 25px;
 `;
 
-export const Options = styled.div`
-  min-height: 50%;
+export const Description = styled.div`
+  width: 90%;
+  margin-bottom: 5%;
+  color: white;
+
+`;
+
+export const Menu = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
   min-width: 90%;
-  border: 2px solid red;
+`;
+
+export const MenuItem = styled.div`
+  margin: 5%;
+
+
 `;
