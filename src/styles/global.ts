06f1f96fb4abf9aaaa90f57f707274a2f0ba66d0
Alex Hauka <alexhauka@gmail.com>
2021-04-15 13:01:41 -0700

    add Monserrat 600 font to global styling

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,7 +7,7 @@ export default createGlobalStyle`
     padding: 0;
     outline: 0;
     box-sizing: border-box;
-    font-family: monospace;
+    font-family: 'Montserrat', sans-serif;
     color: white;
   }
  
