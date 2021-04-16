fb0bcedc3dba38f92acdc852a9ccec2a79c19331
Alex Hauka <alexhauka@gmail.com>
2021-04-16 13:25:20 -0700

    wip lift state for intolerances

--- src/components/Picker/intolerances/index.tsx
+++ src/components/Picker/intolerances/index.tsx
@@ -1,20 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Container, Description } from './styles';
 
-const intolerancesArray: string[] = ["Dairy", "Egg", "Gluten", "Grain", "Peanut", "Seafood", "Sesame", "Shellfish", "Soy", "Sulfite", "TreeNut", "Wheat"];
+const intolerancesArray: string[] = ["dairy", "egg", "gluten", "grain", "peanut", "seafood", "sesame", "shellfish", "soy", "sulfite", "tree-nut", "wheat"];
 
 let checkedIntolerances: string[] = [];
 
-function handleCheckIntolerances(event: React.ChangeEvent<HTMLInputElement>) {
-  
-  if (event.target.checked === true) {
-    checkedIntolerances.push(event.target.name)
-  } else {
-    event.target.checked = false;
-    const i = checkedIntolerances.indexOf(event.target.name);
-    checkedIntolerances.splice(i, 1);
-  }
-}
+
 
 
 
@@ -22,20 +13,34 @@ const Intolerances: React.FC = () => {
 
   const [intolerances, setIntolerances] = useState(checkedIntolerances)
 
+  function handleCheckIntolerances(event: React.ChangeEvent<HTMLInputElement>) {
+  
+    if (event.target.checked === true) {
+      checkedIntolerances.push(event.target.name)
+    } else {
+      // event.target.checked = false;
+      const i = checkedIntolerances.indexOf(event.target.name);
+      checkedIntolerances.splice(i, 1);
+    }
+    setIntolerances(checkedIntolerances);
+    
+  }
+
   return (
     <Container>
       <ul className="columns">
-      {intolerancesArray.map((item: any, index: number) => <li
+      {intolerancesArray.map((item: string, index: number) => <li
       
       key={index}
       className="intolerances-list">
       <input
       name={item}
       type="checkbox"
+      // checked={checkedIntolerances.includes(item) ? true : false}
       onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
         handleCheckIntolerances(e);
-        setIntolerances(checkedIntolerances);
         console.log(intolerances);
+        // console.log(check)
       }}
       className="larger"
       value={item}/>
