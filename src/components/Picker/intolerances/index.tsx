1978af707eac04162e0f23ccbe997fe3ff529641
Alex Hauka <alexhauka@gmail.com>
2021-04-16 12:18:41 -0700

    add checked intolerances to array for processing

--- src/components/Picker/intolerances/index.tsx
+++ src/components/Picker/intolerances/index.tsx
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Description } from './styles';
 
-const intolerancesArray = ["Dairy", "Egg", "Gluten", "Grain", "Peanut", "Seafood", "Sesame", "Shellfish", "Soy", "Sulfite", "Tree Nut", "Wheat"];
+const intolerancesArray: string[] = ["Dairy", "Egg", "Gluten", "Grain", "Peanut", "Seafood", "Sesame", "Shellfish", "Soy", "Sulfite", "TreeNut", "Wheat"];
+
+let checkedIntolerances: string[] = [];
+
+function handleCheckIntolerances(event: React.ChangeEvent<HTMLInputElement>) {
+  
+  if (event.target.checked === true) {
+    checkedIntolerances.push(event.target.name)
+  } else {
+    event.target.checked = false;
+    const i = checkedIntolerances.indexOf(event.target.name);
+    checkedIntolerances.splice(i, 1);
+  }
+}
 
 
 
 const Intolerances: React.FC = () => {
 
+  const [intolerances, setIntolerances] = useState(checkedIntolerances)
+
   return (
     <Container>
       <ul className="columns">
-      {intolerancesArray.map((item, index: number) => <li
+      {intolerancesArray.map((item: any, index: number) => <li
+      
       key={index}
       className="intolerances-list">
       <input
+      name={item}
       type="checkbox"
+      onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+        handleCheckIntolerances(e);
+        setIntolerances(checkedIntolerances);
+        console.log(intolerances);
+      }}
       className="larger"
       value={item}/>
         &nbsp;{item}
