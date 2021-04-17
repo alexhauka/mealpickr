0971ecf7aa65663ef736fbaf44bd062984efb39d
Alex Hauka <alexhauka@gmail.com>
2021-04-17 13:17:43 -0700

    implement checkbox state for intolerances

--- src/components/Picker/intolerances/index.tsx
+++ src/components/Picker/intolerances/index.tsx
@@ -1,30 +1,74 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Description } from './styles';
 
 const intolerancesArray: string[] = ["dairy", "egg", "gluten", "grain", "peanut", "seafood", "sesame", "shellfish", "soy", "sulfite", "tree-nut", "wheat"];
 
 let checkedIntolerances: string[] = [];
 
+// const intolerancesObject: {[index: string]:any} = {
+//   dairy:  { isChecked: false },
+//   egg: { isChecked: false },
+//   gluten: { isChecked: false },
+//   grain: { isChecked: false },
+//   peanut: { isChecked: false },
+//   seafood: { isChecked: false },
+//   sesame: { isChecked: false },
+//   shellfish: { isChecked: false },
+//   soy: { isChecked: false },
+//   sulfite: { isChecked: false },
+//   "tree-nut": { isChecked: false },
+//   wheat: { isChecked: false }
+// }
 
-
+interface itemCheck {
+  [name: string]: {
+    [key: string]: boolean
+  }
+}
 
 
 const Intolerances: React.FC = () => {
 
   const [intolerances, setIntolerances] = useState(checkedIntolerances)
 
+  const [itemChecks, setItemChecks] = useState<itemCheck>({
+    dairy:  { isChecked: false },
+    egg: { isChecked: false },
+    gluten: { isChecked: false },
+    grain: { isChecked: false },
+    peanut: { isChecked: false },
+    seafood: { isChecked: false },
+    sesame: { isChecked: false },
+    shellfish: { isChecked: false },
+    soy: { isChecked: false },
+    sulfite: { isChecked: false },
+    "tree-nut": { isChecked: false },
+    wheat: { isChecked: false }
+  })
+
   function handleCheckIntolerances(event: React.ChangeEvent<HTMLInputElement>) {
-  
+
     if (event.target.checked === true) {
       checkedIntolerances.push(event.target.name)
+      setItemChecks({
+        ...itemChecks,
+        [event.target.name]: { isChecked: true }
+      })
     } else {
-      // event.target.checked = false;
       const i = checkedIntolerances.indexOf(event.target.name);
       checkedIntolerances.splice(i, 1);
+      setItemChecks({
+        ...itemChecks,
+        [event.target.name]: { isChecked: false }
+      })
     }
     setIntolerances(checkedIntolerances);
-    
+    console.log(intolerances)
   }
+  
+    // useEffect(() => {
+      
+    // }, [intolerances, itemChecks])
 
   return (
     <Container>
@@ -36,11 +80,10 @@ const Intolerances: React.FC = () => {
       <input
       name={item}
       type="checkbox"
-      // checked={checkedIntolerances.includes(item) ? true : false}
+      checked={itemChecks[item].isChecked}
       onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
         handleCheckIntolerances(e);
-        console.log(intolerances);
-        // console.log(check)
+        // console.log(intolerances);
       }}
       className="larger"
       value={item}/>
