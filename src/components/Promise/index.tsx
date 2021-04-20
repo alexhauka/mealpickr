0f226b7a4c53f3135be42f4410a5dac39ab4161c
Alex Hauka <alexhauka@gmail.com>
2021-04-19 17:46:33 -0700

    switch privacy promise to MiscContainer resuable

--- src/components/Promise/index.tsx
+++ src/components/Promise/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { ParentContainer, ParagraphDescription } from '../reusables';
+import { MiscContainer, ParagraphDescription } from '../reusables';
 
 const About: React.FC = () => {
   return (
     <>
-      <ParentContainer>
+      <MiscContainer>
         <ParagraphDescription>
           As someone who's incredibly offput by the practices of data collection in the online world, I chose to make this app as clean and safe as possible for the user. I have no control over third party collection from Google and/or the company that provides your internet and can't do anything about that. But for the scope of mealpickr, I made it so no cookies are used or stored, and no data or personal information is collected from my end.
           <br /> Happy eating!
         </ParagraphDescription>
-      </ParentContainer>
+      </MiscContainer>
     </>
   );
 }
